ac3f86d290ea9907d92d475dcb0557aedd5d9ace
SunitCodes <sunitdasclassx@gmail.com>
2024-04-01 21:24:29 +0530

    New Route added

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -5,6 +5,8 @@ const router = express.Router();
 const userRoute = require('../models/user');
 const {jsonAuthMiddleware, generateToken} = require('./../jwt');
 
+
+// SignUp - POST
 router.post('/signup', async function (req, res) {
     try {
         const data = req.body; 
@@ -29,15 +31,44 @@ router.post('/signup', async function (req, res) {
     }
 })
 
-router.get('/signup', async (req, res) => {
-    try {
-        const response = await userRoute.find();
-        console.log("Data Fetched");
-        res.status(200).json(response);
+// router.get('/signup', async (req, res) => {
+//     try {
+//         const response = await userRoute.find();
+//         console.log("Data Fetched");
+//         res.status(200).json(response);
         
-    } catch (err) {
+//     } catch (err) {
+//         console.log("Error occured");
+//         res.status(500).json({ error: "Internal Error Occured" });
+//     }
+// })
+
+
+// Login - POST
+router.post('/login', async function (req, res) {
+    try{
+        const { aadharNumber, password } = req.body;
+
+        //find the user in database and check if password matches
+        const user = await userRoute.findOne({aadharNumber : aadharNumber})
+
+        if(!user || !(await user.comparePassword(password))){
+            return res.status(401).json({ message : "Invalid username or password"})
+        }
+
+        //if password matches - generate token
+        const payload = {
+            id : user.id
+        }
+        const token = generateToken(payload);
+        
+        console.log("Data Saved");
+        console.log("Token generated is :",token);
+        res.status(200).json({token: token});
+
+    }catch(err){
         console.log("Error occured");
-        res.status(500).json({ error: "Internal Error Occured" });
+        res.status(500).json({ error: "Internal Error Occured", err });
     }
 })
 
